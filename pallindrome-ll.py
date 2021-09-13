33102da6fea43bc0d4a55b65734d39e478acabda
Nitesh <34005937+gaddalaynitesh@users.noreply.github.com>
2021-09-13 15:55:32 -0500

    Create pallindrome-ll.py

---
+++ pallindrome-ll.py
@@ -0,0 +1,110 @@
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10**5)
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+def isPalindrome(head) :
+    #Your code goes here
+    nums = []
+    while head:
+        nums.append(head.data)
+        head = head.next
+    l,r = 0 , len(nums) - 1
+    while l<=r:
+        if nums[l]!=nums[r]:
+            return False
+        l+=1
+        r-=1
+    return True
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+#to print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head = takeInput()
+    
+    if isPalindrome(head) :
+        print('true')
+    else :
+        print('false')
+        
+    t -= 1
