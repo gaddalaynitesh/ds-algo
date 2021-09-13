e472a0349ead0e767f97c5c6ad0681efd88a73f6
Nitesh <34005937+gaddalaynitesh@users.noreply.github.com>
2021-09-13 15:21:56 -0500

    Create reverse_ll(recurs).py

---
+++ reverse_ll(recurs).py
@@ -0,0 +1,49 @@
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10 ** 6)
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+
+def printReverse(head) :
+    #Your code goes here
+    if head is None:
+        return 
+    else:
+        curr = head
+        head = printReverse(head.next)
+        print(curr.data,end = " ")
+        
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
